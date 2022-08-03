c0d4547dc92694bee7723c7825bf996bc56b15b3
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-08-03 11:33:08 -0300

    tentando fazer o requisito 6

--- script.js
+++ script.js
@@ -1,12 +1,62 @@
+const getUl = document.querySelectorAll('ul');
 function criaColunasComLi() {
-  let getUl = document.querySelectorAll('ul');
-  for(let index = 0; index < getUl.length; index += 1) {
-    for(let index2 = 0; index2 < getUl.length; index2 += 1) {
+  for (let index = 0; index < getUl.length; index += 1) {
+    for (let index2 = 0; index2 < getUl.length; index2 += 1) {
       let criaLi = document.createElement('li');
+      criaLi.style.backgroundColor = 'white';
       criaLi.className = 'pixel';
+     // criaLi.textContent = index;
       document.querySelectorAll('ul')[index].appendChild(criaLi);
     }
   }
-  let getPixelBoard = document.getElementById('pixel-board');
 }
 criaColunasComLi();
+
+
+// adicionei a classe selected inicialmente na cor black
+const blackColor = document.getElementById('black');
+blackColor.classList.add('selected');
+
+// cria a variavel que recebe a cor
+let selectedColor = document.getElementById('currentColor');
+selectedColor.style.backgroundColor = document.querySelector('.selected').id;
+
+/*
+  let getBlack = document.getElementById('black');
+  getBlack = addEventListener('click', selectColor);
+
+  let getColor2 = document.getElementById('red');
+  getColor2 = addEventListener('click', selectColor);
+
+  let getColor3 = document.getElementById('blue');
+  getColor3 = addEventListener('click', selectColor);
+
+  let getColor4 = document.querySelector('.green');
+  getColor4 = addEventListener('click', selectColor);
+
+function selectColor(color) {
+  console.log(color);
+  console.log(selectedColor);
+  //selectedColor.style.backgroundColor;
+ // console.log(selectedColor);
+  
+  let removeSelected = document.getElementsByClassName('selected');
+  console.log(removeSelected);
+  removeSelected[0].classList.remove('selected');
+
+  color.classList.add('selected');
+  selectedColor.style.backgroundColor = document.querySelector('.selected').id;
+  console.log(color);
+}
+*/
+
+function changeBackgrondColor(currentPixel) {
+  currentPixel.target.style.backgroundColor = selectedColor.style.backgroundColor;
+}
+
+function paintPixels() {
+  let currentPixel = document.querySelectorAll('ul');
+  currentPixel = addEventListener('click', changeBackgrondColor);
+}
+paintPixels();
+
