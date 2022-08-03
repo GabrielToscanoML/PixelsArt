aa3bd78504dd653830f15bda43e2cdf5400d895b
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-08-03 12:54:35 -0300

    ajeitando indentacao

--- script.js
+++ script.js
@@ -2,34 +2,33 @@
 const blackColor = document.getElementById('black');
 blackColor.classList.add('selected');
 
-let currentSelected = document.getElementsByClassName('selected');
+const currentSelected = document.getElementsByClassName('selected');
 
 // cria a variavel que recebe a cor
-let selectedColor = document.getElementById('currentColor');
+const selectedColor = document.getElementById('currentColor');
 selectedColor.style.backgroundColor = document.querySelector('.selected').id;
 
-
-let getBlack = document.getElementById('black');
-getBlack.addEventListener('click', function() {
+const getBlack = document.getElementById('black');
+getBlack.addEventListener('click', function(e) {
   getCurrentColor('black');
 });
 
-let getColor2 = document.getElementById('red');
-getColor2.addEventListener('click', function() {
+const getColor2 = document.getElementById('red');
+getColor2.addEventListener('click', function(e) {
   getCurrentColor('red');
 });
 
-let getColor3 = document.getElementById('blue');
-getColor3.addEventListener('click', function() {
+const getColor3 = document.getElementById('blue');
+getColor3.addEventListener('click', function(e) {
   getCurrentColor('blue');
 });
 
-let getColor4 = document.getElementById('green');
-getColor4.addEventListener('click', function() {
+const getColor4 = document.getElementById('green');
+getColor4.addEventListener('click', function(e) {
   getCurrentColor('green');
 });
 
-function getCurrentColor(identify){
+function getCurrentColor(identify) {
   currentSelected[0].classList.remove('selected');
   document.getElementById(identify).classList.add('selected');
   selectedColor.style.backgroundColor = document.querySelector('.selected').id;
@@ -43,7 +42,7 @@ const getUl = document.querySelectorAll('ul');
 function criaColunasComLi() {
   for (let index = 0; index < getUl.length; index += 1) {
     for (let index2 = 0; index2 < getUl.length; index2 += 1) {
-      let criaLi = document.createElement('li');
+      const criaLi = document.createElement('li');
       criaLi.addEventListener('click', changeBackgrondColor);
       criaLi.style.backgroundColor = 'white';
       criaLi.className = 'pixel';
@@ -52,4 +51,3 @@ function criaColunasComLi() {
   }
 }
 criaColunasComLi();
-
