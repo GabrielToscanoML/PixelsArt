1db1306b05eb55163eadf7dfbcbcaa0a7c1dcc25
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-08-04 19:43:50 -0300

    testando algumas coisas novas

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const boardSize = 5;
-
 // adicionei a classe selected inicialmente na cor black
 const blackColor = document.getElementById('black');
 blackColor.classList.add('selected');
@@ -16,6 +14,7 @@ function getCurrentColor(identify) {
   selectedColor.style.backgroundColor = document.querySelector('.selected').id;
 }
 
+// criando evento para cada cor na paleta de cores
 const getBlack = document.getElementById('black');
 getBlack.addEventListener('click', () => {
   getCurrentColor('black');
@@ -36,12 +35,53 @@ getColor4.addEventListener('click', () => {
   getCurrentColor('green');
 });
 
+// funcao para alterar a cor de fundo de cada pixel
 function changeBackgrondColor(currentPixel) {
   const currentP = currentPixel;
   currentP.target.style.backgroundColor = selectedColor.style.backgroundColor;
 }
 
+// criacao do quadro
+let num;
+
+const btnGenerateBoard = document.getElementById('generate-board');
+btnGenerateBoard.addEventListener('click', () => {
+  let getTextValue = document.getElementById('board-size').value;
+  num = getTextValue;
+  if(getTextValue === '') {
+    window.alert('Board Inválido!');
+    return;
+  }
+  if (getTextValue > 4 && getTextValue < 51){ 
+    criaPixels(getTextValue);
+    return;
+  }
+  if (getTextValue > 50) {
+    getTextValue = 50;
+    criaPixels(getTextValue);
+    return;
+  }
+  if (getTextValue < 5 && getTextValue > 0) {
+    getTextValue = 5;
+    criaPixels(getTextValue);
+    return;
+  }
+});
+
+/*
+function deletePixels(num) {
+  for (let index = 0; index < num * num; index += 1) {
+    const deleteDiv = document.querySelectorAll('.pixel');
+    deleteDiv[index].remove();
+    console.log(document.getElementById('pixel-board'));
+  }
+    
+}
+*/
+
 function criaPixels(num) {
+  document.documentElement.style.setProperty('--boardSizeRows', num);
+  document.documentElement.style.setProperty('--boardSizeColumns', num);
   for (let index = 0; index < num * num; index += 1) {
     const criaDiv = document.createElement('div');
     criaDiv.addEventListener('click', changeBackgrondColor);
@@ -51,10 +91,8 @@ function criaPixels(num) {
   }
 }
 
-criaPixels(boardSize);
-
 function limpaQuadros() {
-  for (let index = 0; index < (boardSize * boardSize); index += 1) {
+  for (let index = 0; index < (num * num); index += 1) {
     const getAllPixels = document.getElementsByClassName('pixel');
     getAllPixels[index].style.backgroundColor = 'white';
   }
